6c3c2cc90350beccbe33d87e7d8ae520a94dbc37
Arielcito <serato.arieli@gmail.com>
2024-10-26 20:49:23 -0300

    remove blog

--- src/sanity/sanity-utils.ts
+++
@@ -1,9 +0,0 @@
-import ImageUrlBuilder from "@sanity/image-url";
-import { createClient, type QueryParams } from "next-sanity";
-import clientConfig from "./config/client-config";
-
-export const client = createClient(clientConfig);
-
-export function imageBuilder(source: string) {
-	return ImageUrlBuilder(clientConfig).image(source);
-}
